dc32a43514b0ceee06fe5feec34795062a32ca07
joaquimserafim <joaquim.serafim@gmail.com>
2016-12-05 22:57:37 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+/*
+eslint
+no-multi-spaces: ["error", {exceptions: {"VariableDeclarator": true}}]
+padded-blocks: ["error", {"classes": "always"}]
+max-len: ["error", 80]
+*/
+'use strict'
+
+const Joi   = require('joi')
+const apply = require('node-apply')
+
+module.exports = ValidateSchema
+
+function ValidateSchema (options) {
+  if (!(this instanceof ValidateSchema)) {
+    return new ValidateSchema(options)
+  }
+
+  this._options = options
+}
+
+ValidateSchema.prototype.query = function query (schema) {
+  return apply(procRequest, 'query', this._options, schema)
+}
+
+ValidateSchema.prototype.params = function params (schema) {
+  return apply(procRequest, 'params', this._options, schema)
+}
+
+ValidateSchema.prototype.body = function body (schema) {
+  return apply(procRequest, 'body', this._options, schema)
+}
+
+ValidateSchema.prototype.headers = function headers (schema) {
+  return apply(procRequest, 'headers', this._options, schema)
+}
+
+ValidateSchema.prototype.response = function response (schema) {
+  return apply(procResponse, this._options, schema)
+}
+
+//
+//
+//
+
+function procRequest (type, options, schema, req, res, next) {
+
+  Joi.validate(req[type], schema, options, validateRequest)
+
+  function validateRequest (err, value) {
+    return err
+      ? res.status(400).send(err.message)
+      : (req[type] = value) && next()
+  }
+}
+
+function procResponse (options, schema, req, res, next) {
+
+  res._json = res.json
+  res.json = jsonMethodOverride
+
+  function jsonMethodOverride (body) {
+
+    Joi.validate(body, schema, options, validateResponse)
+
+    function validateResponse (err) {
+      return err
+        ? res.status(500).end(err.message)
+        : res._json(body)
+    }
+  }
+
+  next()
+}
+
---
+++ test.js
@@ -0,0 +1,181 @@
+/*
+eslint
+no-multi-spaces: ["error", {exceptions: {"VariableDeclarator": true}}]
+padded-blocks: ["error", {"classes": "always"}]
+max-len: ["error", 80]
+*/
+'use strict'
+
+const express     = require('express')
+const request     = require('supertest')
+const bodyParser  = require('body-parser')
+const Joi         = require('joi')
+const mocha       = require('mocha')
+
+const it        = mocha.it
+const describe  = mocha.describe
+const before    = mocha.before
+
+const validateSchema = require('./')
+
+const app = express()
+
+const someSchema = Joi.object().keys(
+  {
+    id: Joi.number().positive().required()
+  }
+)
+
+const headersSchema = Joi.object().keys(
+  {
+    host: Joi.string(),
+    'accept-encoding': Joi.string(),
+    'user-agent': Joi.string(),
+    id: Joi.number().positive().required(),
+    connection: Joi.string().valid('close')
+  }
+)
+
+// const responseSchema = Joi.object().keys(
+//   {
+
+//   }
+// )
+
+describe('express midlleware schema validator', () => {
+
+  describe('request', () => {
+
+    before((done) => {
+      let router = express.Router()
+
+      // query string endpoint
+      router.get(
+        '/querystring',
+        validateSchema().query(someSchema),
+        (req, res) => { res.send('query string') }
+      )
+
+      // params endpoint
+      router.get(
+        '/params/:id',
+        validateSchema().params(someSchema),
+        (req, res) => { res.send('params') }
+      )
+
+      // body endpoint
+      router.post(
+        '/body',
+        validateSchema().body(someSchema),
+        (req, res) => { res.send('body') }
+      )
+
+      // headers endpoint
+      router.get(
+        '/headers',
+        validateSchema().headers(headersSchema),
+        (req, res) => { res.send('headers') }
+      )
+
+      app.use(bodyParser.json())
+      app.use('/request', router)
+
+      done()
+    })
+
+    it('should throw a 400 when fails on the schema validation',
+      (done) => {
+        request(app)
+          .get('/request/querystring?id=bad')
+          .expect(
+            400,
+            'child "id" fails because ["id" must be a number]',
+            done
+          )
+      }
+    )
+
+    it('should return a 200 when validating a valid param(s)', (done) => {
+      request(app)
+        .get('/request/params/123')
+        .expect(200, 'params', done)
+    })
+
+    it('should return a 200 when validating a valid body', (done) => {
+      request(app)
+        .post('/request/body')
+        .send({id: 123})
+        .expect(200, 'body', done)
+    })
+
+    it('should return a 200 when validating a valid header(s)', (done) => {
+      request(app)
+        .get('/request/headers')
+        .set('id', 123)
+        .expect(200, 'headers', done)
+    })
+
+    it('should return a 200 when validating a valid query string', (done) => {
+      request(app)
+        .get('/request/querystring?id=123')
+        .expect(200, 'query string', done)
+    })
+
+    it('should return a 200 when validating a valid param(s)', (done) => {
+      request(app)
+        .get('/request/params/123')
+        .expect(200, 'params', done)
+    })
+
+    it('should return a 200 when validating a valid body', (done) => {
+      request(app)
+        .post('/request/body')
+        .send({id: 123})
+        .expect(200, 'body', done)
+    })
+
+    it('should return a 200 when validating a valid header(s)', (done) => {
+      request(app)
+        .get('/request/headers')
+        .set('id', 123)
+        .expect(200, 'headers', done)
+    })
+  })
+
+  describe('response', () => {
+
+    before((done) => {
+      let router = express.Router()
+
+      router.get(
+        '/good',
+        validateSchema().response(someSchema),
+        (req, res) => { res.send({id: 123}) }
+      )
+
+      router.get(
+        '/bad',
+        validateSchema().response(someSchema),
+        (req, res) => { res.send({id: 'bad'}) }
+      )
+
+      app.use(bodyParser.json())
+      app.use('/response', router)
+
+      done()
+    })
+
+    it('should return a 200 for a valid response', (done) => {
+      request(app)
+        .get('/response/good')
+        .expect(200, {id: 123}, done)
+    })
+
+    it('should throw a 500 for an invalid response', (done) => {
+      request(app)
+        .get('/response/bad')
+        .expect(500, 'child "id" fails because ["id" must be a number]', done)
+
+    })
+  })
+})
